3cef6c8dbeca01dac76b5bee30c45f39a847989c
Tien Phong <tienphong2203@gmail.com>
2024-11-19 15:30:28 +0700

    Step 8: Create about user

--- src/modules/user/about/about.component.ts
+++ src/modules/user/about/about.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  public logo: string = './assets/images/logo.png'
+  public textLogo: string = 'Quizzes'
+  public quiz01: string = './assets/images/quiz-bg-01.jpeg'
 
+  public member01: string = './assets/images/avatar-2.png'
+  public member02: string = './assets/images/avatar-7.png'
+  public member03: string = './assets/images/avatar-9.png'
 }
