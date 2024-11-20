50efce1a9576e729d763c75851497366e78a8841
Tien Phong <tienphong2203@gmail.com>
2024-11-20 10:32:08 +0700

    Step 10: Config to user contact and quizzes

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,11 +3,22 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { QuizzesComponent } from './quizzes/quizzes.component';
 
 const routes: Routes = [
   {
     path: 'about',
     component: AboutComponent
+  },
+
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'quizzes',
+    component: QuizzesComponent
   },
 
   {
