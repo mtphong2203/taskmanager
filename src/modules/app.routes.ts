1e0f9ea5f770d1f41d3fc5368a1c8134bbaa2de8
Tien Phong <tienphong2203@gmail.com>
2024-11-20 14:20:56 +0700

    Step 13: Config manager url

--- src/modules/app.routes.ts
+++ src/modules/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { UserLayoutComponent } from './shared/user-layout/user-layout.component';
+import { ManagerLayoutComponent } from './shared/manager-layout/manager-layout.component';
 
 export const routes: Routes = [
+    {
+        path: 'manager',
+        component: ManagerLayoutComponent
+    },
+
     {
         path: '',
         component: UserLayoutComponent,
